1738037406e3ec53b84b4037af5deb487c3cc723
namigismylvv <11869@holbertonstudents.com>
2025-08-08 13:50:43 +0000

    feat

---
+++ markdown2html.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that converts a Markdown file to HTML.
+
+Usage:
+    ./markdown2html.py input.md output.html
+"""
+
+import sys
+import os
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    md_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not os.path.isfile(md_file):
+        print(f"Missing {md_file}", file=sys.stderr)
+        sys.exit(1)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
